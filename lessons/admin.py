39cfd7951828acecb6ef46dac13109486764adb2
YUSHENG LU <yusheng.lu@kcl.ac.uk>
2022-11-18 15:21:55 +0000

    Add pk in admin page, delete useless migrations

--- lessons/admin.py
+++ lessons/admin.py
@@ -9,9 +9,13 @@ admin.site.enable_nav_sidebar = False
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
     """Configuration fo the admin interface for users."""
-    list_display = ('email', 'first_name', 'last_name', 'is_staff', 'is_superuser', 'is_active')
+    list_display = ('email', 'first_name', 'last_name', 'is_staff', 'is_superuser', 'is_active','primary_key')
     fields = ('email', 'first_name', 'last_name', 'is_staff', 'is_superuser', 'is_active')
-    
+
+    def primary_key(self,value):
+        return value.pk
+    primary_key.short_description = 'student number'
+
 @admin.register(Lesson)
 class LessonAdmin(admin.ModelAdmin):
      list_display = ('id', 'is_confirmed')
