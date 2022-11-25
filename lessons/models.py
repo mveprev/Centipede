b3413422452f80b81878e0b62ae3c4d751837f33
AbdulAdal <k21007037@kcl.ac.uk>
2022-11-25 15:03:19 +0000

    Implement lessons table field

--- lessons/models.py
+++ lessons/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 from django.contrib.auth.base_user import AbstractBaseUser, BaseUserManager
 from django.contrib.auth.models import PermissionsMixin, User
+from django.core.validators import MaxValueValidator, MinValueValidator 
 from msms import settings
 # Create your models here.
 
@@ -64,10 +65,10 @@ class Children(models.Model):
     parent = models.ForeignKey(User, on_delete=models.CASCADE)
 
 class Lesson(models.Model):
-    availabilityDay = models.IntegerField(blank=False)
-    availabilityTime = models.IntegerField(blank=False)
-    lessons = models.IntegerField(blank=False)
-    desiredInterval = models.IntegerField(blank=False)
+    availabilityDay = models.CharField(max_length=100, blank=False)
+    availabilityTime = models.CharField(max_length=100, blank=False)
+    lessons = models.IntegerField(validators=[MaxValueValidator(4)],blank=False)
+    desiredInterval = models.CharField(max_length=100, blank=False)
     duration = models.IntegerField(blank=False)
     furtherInfo = models.TextField()
     id = models.AutoField(primary_key=True, unique=True)
